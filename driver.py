24f8702a572589a02996ec20bcf11726029b5947
Sean Breckenridge <seanbrecke@gmail.com>
2019-07-17 12:19:31 -0700

    add logging, dont wait for downloads before going to next page
    can go to next page when file count increases,
    don't have to wait for downlaods to finish
    
    also added some error checks for forms opening by mistake
    and using the number of images (by selecting 'a' elements)
    on the album page to help with edge cases

--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ import sys
 import glob
 import re
 import shutil
+import logging
 from time import sleep
 from random import randint, shuffle
 
@@ -16,7 +17,7 @@ from selenium.common.exceptions import StaleElementReferenceException, NoSuchEle
 
 driver = None
 wait = None
-fast_wait = None
+logging.basicConfig(level=logging.INFO)
 
 def wait_random():
     """Wait for time between keystrokes."""
@@ -29,21 +30,6 @@ def enter_text_slow(box, text):
         box.send_keys(c)
         wait_random()
 
-
-def check_if_on_last_page(albumpath):
-    # check if we're on the last page by looking at the 'x of y' (e.g. '1 of 12') description text
-    try:
-        x_of_y = driver.find_element_by_css_selector('#pic-detail-page-footer span.detail-paging-page-num')
-        x, y = re.findall("(\d+)\s+of\s+(\d+)", x_of_y.text)[0]
-
-        # if 'x of y''s y equals the number of images we have downloaded, skip this album
-        if int(y) == len([f for f in os.listdir(albumpath) if not f.startswith('.')]):
-            print(f"Already downloaded everything from {albumname}, skipping")
-            return True
-
-        return x == y
-    except NoSuchElementException:
-        return True # only one image in the album
 
 
 def go_to_next_page():
@@ -71,7 +57,7 @@ def go_to_next_page():
               hide_input=True, confirmation_prompt=True)
 @click.option("--chromedriverpath", help="path to chromedriver", default="/usr/local/bin/chromedriver")
 def main(base_shutterfly_url, username, password, chromedriverpath):
-    global driver, wait, fast_wait
+    global driver, wait, wait
 
     # x-y coords of download button
     download_x, download_y = None, None
@@ -84,25 +70,25 @@ def main(base_shutterfly_url, username, password, chromedriverpath):
 
     # create dirs
     if not os.path.exists(watch_tmp_dir):
+        logging.info(f"Making {watch_tmp_dir}")
         os.makedirs(watch_tmp_dir)
 
     # remove files from previous runs
     for leftover_files in glob.glob(f'{watch_tmp_dir}/*'):
+        logging.info(f"Removing {leftover_files} from previous run")
         os.remove(leftover_files)
-
 
     # change download directory
     chromeOptions = webdriver.ChromeOptions()
     prefs = {"download.default_directory" : watch_tmp_dir}
     chromeOptions.add_experimental_option("prefs",prefs)
     driver = webdriver.Chrome(executable_path=chromedriverpath, options=chromeOptions)
-    wait = WebDriverWait(driver, 15)
-    fast_wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 30)
 
     # go to albums page
     driver.get("{}/pictures".format(base_shutterfly_url.rstrip("/")))
 
-
+    logging.info("Logging in")
     # wait to login
     wait.until(EC.presence_of_element_located((By.ID, "signinForm")))
     form = driver.find_element_by_id("signinForm")
@@ -115,29 +101,50 @@ def main(base_shutterfly_url, username, password, chromedriverpath):
     view_albums_url = driver.find_element_by_partial_link_text("View all").get_attribute('href')
     driver.get(view_albums_url)
 
-    # wait to click on 'All albums' link
+
+    initial_album_count = len(driver.find_elements_by_css_selector("div.pic-album-ftr > a"))
+
+    # change tabs to make sure all albums are visible
     try:
-        fast_wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#pic-albumDetail-left div.navbar-left a[onclick*="All"]')))
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#pic-albumDetail-left div.navbar-left a[onclick*="All"]')))
+        driver.find_element_by_css_selector('#pic-albumDetail-left div.navbar-left a[onclick*="All"]').click()
+
+        # wait until All albums are loaded
+        wait_time = 20
+        while len(driver.find_elements_by_css_selector("div.pic-album-ftr > a")) == initial_album_count:
+            sleep(1)
+            logging.info('.')
+        else:
+            logging.warning("Album count didn't increase!")
     except:
         pass # assume there are less than 50 albums, which is whats required for there to be an all button
-    view_all_albums = driver.find_element_by_css_selector('#pic-albumDetail-left div.navbar-left a[onclick*="All"]').click()
-
-    # wait until All albums are loaded
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#pic-albumDetail-left div.navbar-center > div')))
 
     # get all albums
     view_all_albums_elements = driver.find_elements_by_css_selector("div.pic-album-ftr > a")
     all_album_links = [el.get_attribute('href') for el in view_all_albums_elements]
 
+    logging.info(f"Found {len(all_album_links)} albums")
+
     for albumlink in all_album_links:
 
         #  go to album page
+        logging.info(f"Going to {albumlink}")
         driver.get(albumlink)
+
+
+        # count number of images on this page
+        viewable_count = len(driver.find_elements_by_css_selector(".pic-grid img"))
+        logging.info(f"Found {viewable_count} images")
+
+        # get album name
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'title-text')))
+        albumname = driver.find_element_by_class_name('title-text').text.strip()
+        logging.info(f"Album name: {albumname}")
 
         # go to first image in album
         driver.find_element_by_css_selector(".pic-grid img").click()
+        logging.info(f"Clicking on first picture")
 
-        albumname = driver.find_element_by_css_selector('#bc-list-2 > span:nth-child(3) > a').text
         albumpath = os.path.join(download_to, albumname)
 
         if not os.path.exists(albumpath):
@@ -148,80 +155,82 @@ def main(base_shutterfly_url, username, password, chromedriverpath):
             input("Hit enter, then move the mouse to the center of the download button (the top right corner of the image) on the screen (you'll have 5 seconds)")
             sleep(5)
             download_x, download_y = pyautogui.position()
-
+            print(f"Download button position: ({download_x}, {download_y})")
 
         on_last_page = False
 
         while not on_last_page:
 
             # update loop condition
-            on_last_page = check_if_on_last_page(albumpath)
 
+            # if there was only one image, we have to be on the last page
+            if viewable_count == 1:
+                on_last_page = True
+                logging.info("Was only one image, setting on_last_page to True")
+            else:
+                # get the 'x of y', e.g. '1 of 12' text from the bottom of the page
+                x_of_y = driver.find_element_by_css_selector('#pic-detail-page-footer span.detail-paging-page-num')
+                x, y = re.findall("(\d+)\s+of\s+(\d+)", x_of_y.text)[0]
+                on_last_page = x == y
+                logging.info(f"At '{x_of_y.text}': {x}, {y} in this album")
 
-            # check if file already exists
-            possible_filepaths = [os.path.join(
-                                    albumpath,
-                                    driver.find_element_by_id('bc-leaf').text.strip()
-                                    )]
+            logging.info(f"On last page: {on_last_page}")
 
-            # other possible filepaths
-            # shutterfly adds a jpg/jpeg if its not a valid file
-            possible_filepaths.extend([
-                f"{possible_filepaths[0]}.jpg",
-                f"{possible_filepaths[0]}.jpeg"
-            ])
+            # if 'x of y''s y equals the number of images we have downloaded, skip this album
+            images_in_album_already_downloaded = [f for f in os.listdir(albumpath) if not f.startswith('.')]
+            logging.info(f"Album folder currently has {len(images_in_album_already_downloaded)} items.")
+            if (on_last_page and viewable_count == 1 and viewable_count == len(images_in_album_already_downloaded)) or int(y) <= len(images_in_album_already_downloaded):
+                logging.info(f"Already downloaded everything from {albumname}, skipping")
+                break
 
-            print(possible_filepaths)
+            name = driver.find_element_by_id('bc-leaf').text
+            logging.info(f"Name of image: {name}")
 
-            # if the file already exists
-            file_has_been_downloaded = any(map(os.path.exists, possible_filepaths))
+            # watch directory for downloaded file
+            file_count = len(os.listdir(watch_tmp_dir))
+            logging.info(f"Prev file count: {file_count}")
+            for s in range(300):
+                logging.info('.')
+                sleep(1)
+                if s % 30 == 0:
+                    # shake mouse on download button location to make sure its selected
+                    pyautogui.moveTo(download_x, 0)
+                    pyautogui.moveTo(download_x, download_y, duration=0.5)
+                    pyautogui.move(3, 0)
+                    pyautogui.move(-3, 0)
+                    pyautogui.click()
+                if s % 7 == 2:
 
-            if file_has_been_downloaded:
-                print("Downloaded in a previous run")
-                go_to_next_page()
-                continue
+                    # close the tags form if its opened by mistake
+                    try:
+                        close_form = driver.find_element_by_css_selector('div.dlg-buttons > input')
+                        logging.info("Found a tag form, closing")
+                        close_form.click()
+                    except NoSuchElementException:
+                        pass
 
-            while not file_has_been_downloaded:
-
-                # if the file already exists
-                file_has_been_downloaded = any(map(os.path.exists, possible_filepaths))
-
-                # watch directory for downloaded file
-                original_contents = [f for f in os.listdir(watch_tmp_dir) if not f.startswith('.')]
-                for s in range(50):
-                    sleep(1)
-                    if s % 15 == 0:
-                        pyautogui.moveTo(download_x, download_y)
-                        # shake mouse on download button location to make sure its selected
-                        pyautogui.move(0, 3)
-                        pyautogui.move(0, -3)
-                        pyautogui.move(3, 0)
-                        pyautogui.move(-3, 0)
-                        pyautogui.click()
-                    # ignore hidden files or files currently being downloaded
-                    new_contents = [f for f in os.listdir(watch_tmp_dir) if not f.startswith('.') and not f.endswith('.crdownload')]
-                    print(new_contents)
-                    new_files = set(new_contents) - set(original_contents) # set difference
-                    if new_files:
-                        new_file = list(new_files)[0]
-                        print(new_file)
-                        new_file_fullpath = os.path.join(watch_tmp_dir, new_file)
-                        this_filepath = os.path.join(albumpath, new_file)
-                        if this_filepath not in possible_filepaths:
-                            print(f"Unpexpected Filepath! Could not find {this_filepath} in \n{os.linesep.join(possible_filepaths)}")
-                        try:
-                            shutil.move(new_file_fullpath, albumpath)
-                            # TODO: update metadata, file still has metadata
-                        except shutil.Error as se:
-                            # file already downloaded from previous run, somehow
-                            if str(se).startswith('Destination path'):
-                                os.remove(new_file_fullpath)
-                            else:
-                                raise se
+                if len(os.listdir(watch_tmp_dir)) > file_count:
+                    logging.info(f"File count is higher than {file_count}, item has started downloading")
+                    # if on the last page
+                    if on_last_page:
+                        # wait for downloads to finish
+                        logging.info("On last page, waiting for files to finish downloading")
+                        wait_for_downloads = 300
+                        while wait_for_downloads > 0:
+                            sleep(1)
+                            wait_for_downloads -=1
+                            if not any([f.endswith('.crdownload') for f in os.listdir(watch_tmp_dir)]):
+                                logging.info("Files finished downloading")
+                                for f in os.listdir(watch_tmp_dir):
+                                    shutil.move(os.path.join(watch_tmp_dir, f), albumpath)
+                                wait_for_downloads = -1
+                        break
+                    else:
+                        logging.info("Going to next page")
                         go_to_next_page()
                         break
-                else:
-                    print(f"Couldnt download image for album {albumname} at URL: ", webdriver.current_url, file=sys.stderr)
+
+
 
 
 if __name__ == "__main__":
